4e3d497cc070d38979333f8272ff358b42c2642f
dmosyash <kochar.yash@gmail.com>
2019-03-04 16:43:39 +0530

    first block

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+namespace trial {
+
+    /**
+         * Drives forwards. Call stop to stop
+         */
+    //% blockId=servos_forward
+    //% block="forward"
+    export function forward(): void {
+        pins.servoWritePin(AnalogPin.P1, 0);
+        pins.servoWritePin(AnalogPin.P2, 180);
+    }
+}
